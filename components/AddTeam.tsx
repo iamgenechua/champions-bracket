a70ea47f826953805c80be54eccf439773b3e947
iamngenechua <iamgenechua@gmail.com>
2022-08-10 15:53:13 -0700

    Handle frontend input validation for add team

--- components/AddTeam.tsx
+++ components/AddTeam.tsx
@@ -1,42 +1,88 @@
 import React from 'react'
-import { Button, Input } from 'antd'
+import { Button, Input, message } from 'antd'
 import { sendPostRequest } from '../utils';
 import { ADD_TEAM_ENDPOINT } from '../routes';
 
 const { Search } = Input;
 
 const AddTeam = () => {
+
+    const addTeamInstructions = '<Team name> <Team registration date in DD/MM> <Team group number (either 1 or 2)>';
+
+    const validateDate = (dateInput: string) => {
+        const date = dateInput.split('/');
+        if (date.length !== 2) {
+            message.error(`Input not in format: DD/MM`);
+            return {validDate: false};
+        }
+
+        const day = parseInt(date[0]);
+        const month = parseInt(date[1]);
+        const currentYear = new Date().getFullYear();
+
+        const parsedDate = new Date(`${currentYear}/${month}/${day}`);
+        if (isNaN(parsedDate.getTime())) {
+            message.error(`Invalid date: ${dateInput}`);
+            return {validDate: false};
+        }
+
+        return {validDate: true, parsedDate};
+    }
+
+    const validateGroupNumber = (groupNumberInput: string) => {
+        if (groupNumberInput !== '1' && groupNumberInput !== '2') {
+            message.error(`Invalid group number: ${groupNumberInput}`)
+            return {validGroupNumber: false};
+        }
+
+        return { validGroupNumber: true, groupNumber: parseInt(groupNumberInput) };
+    }
+
     const validateInput = (input: string) => {
         const inputArray = input.split(' ');
 
         if (inputArray.length !== 3) {
-            return false;
+            message.error(`Input not in format: ${addTeamInstructions}`)
+            return {validInput: false};
         }
+
+        // destructuring assignment from validateDate and validateGroupNumber
+        const {validDate, parsedDate} = validateDate(inputArray[1]);
+        const {validGroupNumber, groupNumber} = validateGroupNumber(inputArray[2]);
+        if (!validDate || !validGroupNumber) {
+            return {validInput: false};
+        }
+
+        return {validInput: true, teamName: inputArray[0], registrationDate: parsedDate, groupNumber};
     }
 
     const handleSubmitTeam = async (value: string) => {
 
-        // split value into array
-        const inputArray = value.split(' ');
+        const {validInput, teamName, registrationDate, groupNumber} = validateInput(value);
+
+        if (!validInput) {
+            return;
+        }
         
         const data = {
-            teamName: value
-        }
+            teamName,
+            registrationDate,
+            groupNumber
+        };
 
         try {
             await sendPostRequest(ADD_TEAM_ENDPOINT, data);
+            message.success(`Team ${teamName} added successfully`);
         } catch (error) {
-            console.log(error);
+            message.error(`Error adding team: ${error}`);
         }
-
     }
 
     return (
         <>
             <Search
-                placeholder='<Team name> <Team registration date in DD/MM> <Team group number (either 1 or 2)>'
+                placeholder= {addTeamInstructions}
                 enterButton='Add team' onSearch={handleSubmitTeam} />
-            <Input.Group><Input /><Button></Button></Input.Group>
         </>
     )
 }
